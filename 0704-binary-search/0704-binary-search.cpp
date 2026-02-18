a113df8f88851c9216074f4e679c5f6a0b053930
Nitesh Kumar <166297874+niteshg97@users.noreply.github.com>
2026-02-18 22:40:22 +0530

    Time: 0 ms (100%), Space: 31.3 MB (80.06%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,27 @@
+// this solution is for if there is duplicate target element elements...
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int  n =nums.size();
+        int lo=0;
+        int hi=n-1;
+        int ans=-1;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(nums[mid]==target){
+                ans=mid;
+                hi=mid-1;
+
+            }
+            else if(nums[mid]>target){
+                hi=mid-1;
+            }
+            else{
+                lo=mid+1;
+            }
+        }
+        return ans;
+        
+    }
+};
