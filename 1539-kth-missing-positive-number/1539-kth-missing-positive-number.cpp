1a72a60f1c41faa00abc259de654ddfd00358507
Nitesh Kumar <166297874+niteshg97@users.noreply.github.com>
2026-02-21 22:54:52 +0530

    Time: 0 ms (100%), Space: 13.3 MB (28.23%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,17 +1,18 @@
-// lets solve uisng map ... then we will optimized for log(n);
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        unordered_set<int> present(arr.begin(), arr.end());
-        
-        int current = 1;
-        while (k > 0) {
-            if (present.find(current) == present.end()) {
-                k--;
-                if (k == 0) return current;
+        int n =arr.size();
+
+        for(int i=0;i<n;i++){
+            if(arr[i]<=k){
+                k++;
             }
-            current++;
+            else{
+                break;
+            }
+
         }
-        return -1;
+        return k;
+        
     }
 };
