7f39bea0be2058c9336a33e21702e62de8ed20de
Nitesh Kumar <166297874+niteshg97@users.noreply.github.com>
2026-02-23 23:10:41 +0530

    Time: 0 ms (100%), Space: 11.4 MB (37.77%) - LeetHub

---
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == NULL || head->next == NULL) return head;  // base case
+        ListNode* newHead = reverseList(head->next); // recursive call
+        head->next->next = head;
+        head->next = NULL;
+        return newHead;
+    }
+
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if (left == right) return head;
+        ListNode* a = NULL;
+        ListNode* b = NULL;
+        ListNode* c = NULL;
+        ListNode* d = NULL;
+        ListNode* temp = head;
+        int n = 1;
+        while (temp != NULL) {
+            if (n == left - 1) a = temp;
+            if (n == left) b = temp;
+            if (n == right) c = temp;
+            if (n == right + 1) d = temp;
+            temp = temp->next;
+            n++;
+        }
+        if (a) a->next = NULL;
+        c->next = NULL;
+
+        c = reverseList(b);
+
+        if (a) a->next = c;
+        b->next = d;
+
+        if (a) return head;
+        return c;
+    }
+};
