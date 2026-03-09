7f764b8c459d23ad502c99ac13e4f03ae65363b7
Nitesh Kumar <166297874+niteshg97@users.noreply.github.com>
2026-03-09 10:48:39 +0530

    Time: 0 ms (100%), Space: 13.4 MB (28.4%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,21 +1,18 @@
-// this solution using  binary search ..... time complexity is O(log(n))
-// i have alreade solved this in o(n) , time complexity ..
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        int n=arr.size();
-        int lo=0;
-        int hi=n-1;
-        while(lo<=hi){
-            int mid=lo+(hi-lo)/2;
-            int missingNo=arr[mid]-(mid+1);
-            if(missingNo<k){
-                lo=mid+1;
+        int n =arr.size();
+
+        for(int i=0;i<n;i++){
+            if(arr[i]<=k){
+                k++;
             }
             else{
-                hi=mid-1;
+                break;
             }
+
         }
-        return k+hi+1;
+        return k;
+        
     }
 };
