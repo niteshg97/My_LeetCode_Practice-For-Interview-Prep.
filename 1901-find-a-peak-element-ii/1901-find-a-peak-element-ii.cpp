468df41adfbdc707fb5f4cab41f082b3ae236900
Nitesh Kumar <166297874+niteshg97@users.noreply.github.com>
2026-03-14 10:16:10 +0530

    Time: 0 ms (100%), Space: 50.5 MB (69.65%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -1,24 +1,45 @@
-// m-1 Brute force Approach ..we will optimixe latter 
-// T.c O(4 n*m) ~ o(n*m) --> wrost case scenario..
-
+// mathod 2: optmized  solution 
 class Solution {
 public:
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
-        int n =mat.size();
-        int m=mat[0].size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                int up=(i>0) ? mat[i-1][j] : -1;
-                int down=(i<n-1) ? mat[i+1][j] : -1;
-                int left=(j>0) ? mat[i][j-1] :-1;
-                int right=(j<m-1) ? mat[i][j+1] :-1;
 
-                if(mat[i][j]>up && mat[i][j]>down && mat[i][j]>left && mat[i][j]>right){
-                    return {i,j};
+        int n = mat.size();
+        int m = mat[0].size();
+
+        int lo = 0;
+        int hi = m - 1;
+
+        while(lo <= hi){
+
+            int mid = lo + (hi - lo) / 2;
+
+            // find row index of maximum element in mid column
+            int maxRow = 0;
+            for(int i = 0; i < n; i++){
+                if(mat[i][mid] > mat[maxRow][mid]){
+                    maxRow = i;
                 }
             }
+
+            int left  = (mid > 0) ? mat[maxRow][mid-1] : -1;
+            int right = (mid < m-1) ? mat[maxRow][mid+1] : -1;
+
+            // peak found
+            if(mat[maxRow][mid] > left && mat[maxRow][mid] > right){
+                return {maxRow, mid};
+            }
+
+            // move left
+            else if(left > mat[maxRow][mid]){
+                hi = mid - 1;
+            }
+
+            // move right
+            else{
+                lo = mid + 1;
+            }
         }
+
         return {-1,-1};
-        
     }
 };
