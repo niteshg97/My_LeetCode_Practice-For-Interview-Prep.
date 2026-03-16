798bfe5168f304d68cbb5aa160c723c6a455d3fa
Nitesh Kumar <166297874+niteshg97@users.noreply.github.com>
2026-03-16 10:58:21 +0530

    Time: 0 ms (100%), Space: 9.1 MB (98.59%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -3,7 +3,7 @@ public:
     bool isIsomorphic(string s, string t) {
         int n=s.size();
         int m=t.size();
-        vector<int>s1(256,-1);
+        vector<int>s1(256,-1); // creating the two array of size = size of ASCII char..and the filling -1 , in it .
         vector<int>t1(256,-1);
         for(int i=0;i<n;i++){
             if(s1[s[i]]!=t1[t[i]]){
