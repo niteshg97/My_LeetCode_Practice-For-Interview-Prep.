ec6474fbb9baf67acab0255719f7d8037b609b19
Nitesh Kumar <166297874+niteshg97@users.noreply.github.com>
2026-03-16 12:01:49 +0530

    Time: 0 ms (100%), Space: 8.1 MB (84.45%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,21 +1,21 @@
+// method 2 : To Solve this Problem ...
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        int n = s.size();
-        int m =goal.size();
+        int n =s.size();
+        int m=goal.size();
         if(n!=m){
             return false;
         }
-        for(int i=0;i<n;i++){
-            char first=s[0];
-            string rest=s.substr(1);
-            s=rest+first;
-            if(s==goal){
+        string temp=s+s;
+        if(temp.find(goal)!= string::npos){ 
             return true ;
-            }
-        }
 
+        }
         return false ;
-                
+        
+        
     }
 };
+// remember this syntax for string { string::npos  meanns "not found"}
+// This line is checking whether goal exists inside temp
