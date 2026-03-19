45d0b7bf024433c9d15dd07240a23e197dba04bf
Nitesh Kumar <166297874+niteshg97@users.noreply.github.com>
2026-03-19 10:11:01 +0530

    Time: 10 ms (30.39%), Space: 13.2 MB (50.88%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -10,22 +10,17 @@ public:
         mp['C'] = 100;
         mp['D'] = 500;
         mp['M'] = 1000;
-
-        int total = 0;
-        int prev = 0;
-
-        for (int i = n - 1; i >= 0; i--) {
-            int curr = mp[s[i]];
-
-            if (curr < prev) {
-                total -= curr;
-            } else {
-                total += curr;
+        int total=0;
+        for( int i=0;i<n;i++){
+            if(i<n-1 && mp[s[i]]<mp[s[i+1]]){
+                total=total-mp[s[i]];
             }
-
-            prev = curr;
+            else{
+                total=total+mp[s[i]];
+            }
         }
-
         return total;
+
+       
     }
 };
