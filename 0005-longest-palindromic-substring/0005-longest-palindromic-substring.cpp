10cf42030713131ff4dbeb42287c986f90f9c363
Nitesh Kumar <166297874+niteshg97@users.noreply.github.com>
2026-03-20 12:19:42 +0530

    Time: 87 ms (32.25%), Space: 174 MB (14.11%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -12,7 +12,6 @@ public:
         for (int i = 0; i < s.size(); i++) {
             // odd length
             string odd = expand(s, i, i);
-
             // even length
             string even = expand(s, i, i + 1);
             if (odd.size() > ans.size()) ans = odd;
