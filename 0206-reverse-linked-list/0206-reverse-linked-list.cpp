a415d9c250b02781d3d0550eece163ba3692a672
Nitesh Kumar <166297874+niteshg97@users.noreply.github.com>
2026-03-27 15:36:48 +0530

    Time: 0 ms (100%), Space: 13.5 MB (41.79%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -8,27 +8,18 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
- // Method_1: Stack Method 
- // traverse through. the linkedlist and put into the Stack and agin put the Elements from the stack replacing the elements in the Linkedlist: 
-
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         ListNode* temp=head;
-        stack<int>st; //FIFO-First in and the first out 
+        ListNode* prev=NULL;
         while(temp!=NULL){
-            st.push(temp->val);
-            temp=temp->next;
+           ListNode*  front= temp->next;
+            temp->next=prev;
+            prev=temp;
+            temp=front;
         }
-        // again taking from the stack and putting back to the LL
-        temp= head;
-        while(temp!=NULL){
-            temp->val=st.top();
-            st.pop();
-            temp=temp->next;
-        }
-        return head;
+        return prev;
         
     }
 };
