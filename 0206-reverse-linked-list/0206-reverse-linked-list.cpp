44a88ba8459a3176751ee8a3161f58eebe4d9e55
Nitesh Kumar <166297874+niteshg97@users.noreply.github.com>
2026-03-27 15:25:48 +0530

    Time: 0 ms (100%), Space: 13.6 MB (17.02%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -1,18 +1,34 @@
-// Famous question (remmember this approach to solve this problem)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+ // Method_1: Stack Method 
+ // traverse through. the linkedlist and put into the Stack and agin put the Elements from the stack replacing the elements in the Linkedlist: 
 
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* prev=NULL;
-        ListNode* curr=head;
-        ListNode* Next=head;
-        while(curr!=NULL){
-            Next=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=Next;
+        ListNode* temp=head;
+        stack<int>st; //FIFO-First in and the first out 
+        while(temp!=NULL){
+            st.push(temp->val);
+            temp=temp->next;
         }
-        return prev;
+        // again taking from the stack and putting back to the LL
+        temp= head;
+        while(temp!=NULL){
+            temp->val=st.top();
+            st.pop();
+            temp=temp->next;
+        }
+        return head;
         
     }
 };
