5c152f37d7a182498aa529b993d46563c115e60b
Nitesh Kumar <166297874+niteshg97@users.noreply.github.com>
2026-03-28 10:54:29 +0530

    Time: 20 ms (5.4%), Space: 15 MB (5.51%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -1,14 +1,26 @@
-// best method to solve this problem
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+ // Method:1 to solve using the hashmap data structure.
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode* slow=head;
-        ListNode* fast=head;
-    while(fast!=NULL && fast->next!=NULL){
-        slow=slow->next;
-        fast=fast->next->next;
-        if(slow==fast)return true;
-    }
-    return false;
+        map<ListNode* ,int>mpp;
+        ListNode* temp=head;
+        while(temp!=NULL){
+            if(mpp.find(temp)!=mpp.end()){
+                return true;
+            }
+            mpp[temp]++;
+            temp=temp->next;
+        }
+        return false;
+        
     }
 };
