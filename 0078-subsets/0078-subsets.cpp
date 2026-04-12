08b4b7eed309af5230efc83cd8ab9ba24e0f8da2
Nitesh Kumar <166297874+niteshg97@users.noreply.github.com>
2026-04-12 11:47:32 +0530

    Time: 1 ms (42.85%), Space: 9.9 MB (80.74%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,20 +1,20 @@
+// using the Bit manipulation method 
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> result;
-        vector<int> current;
-        backtrack(nums, 0, current, result);
-        return result;
-    }
-
-private:
-    void backtrack(vector<int>& nums, int index, vector<int>& current, vector<vector<int>>& result) {
-        result.push_back(current);  
-
-        for (int i = index; i < nums.size(); ++i) {
-            current.push_back(nums[i]);            
-            backtrack(nums, i + 1, current, result); 
-            current.pop_back();                    
+        vector<vector<int>>ans;
+        int n =nums.size();
+        int subset=1<<n; // = 2^n
+        for(int i=0;i<subset;i++){
+            vector<int>v;
+            for(int j=0;j<n;j++){
+                if(i & (1<<j)){
+                    v.push_back(nums[j]);
+                }
+            }
+            ans.push_back(v);
         }
+        return ans;
+        
     }
 };
