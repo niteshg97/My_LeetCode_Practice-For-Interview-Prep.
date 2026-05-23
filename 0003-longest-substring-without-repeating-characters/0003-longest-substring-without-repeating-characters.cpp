71be0c983827e4945bf3ff19ac06fa4c28985b33
Nitesh Kumar <166297874+niteshg97@users.noreply.github.com>
2026-05-23 18:11:41 +0530

    Time: 8 ms (56.78%), Space: 11.5 MB (85.99%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -6,22 +6,15 @@ public:
         int maxlen = 0;
 
         for (int i = 0; i < n; i++) {
-
             int hash[256] = {0};
-
             for (int j = i; j < n; j++) {
-
                 if (hash[s[j]] == 1)
                     break;
-
                 hash[s[j]] = 1;
-
                 int len = j - i + 1;
-
                 maxlen = max(maxlen, len);
             }
         }
-
         return maxlen;
     }
 };
