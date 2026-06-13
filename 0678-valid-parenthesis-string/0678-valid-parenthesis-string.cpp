4d923abfc61d4bd799817e7e09234b48e37437d5
Nitesh Kumar <166297874+niteshg97@users.noreply.github.com>
2026-06-13 09:06:40 +0530

    Time: 0 ms (100%), Space: 8.1 MB (45.38%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int n = s.size();
+        stack<int> openSt;
+        stack<int> astriskSt;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '(') {
+                openSt.push(i);
+            } else if (s[i] == '*') {
+                astriskSt.push(i);
+            } else {
+                if (!openSt.empty()) {
+                    openSt.pop();
+                } else if (!astriskSt.empty()) {
+                    astriskSt.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        while (!openSt.empty() && !astriskSt.empty()) {
+            if (openSt.top() > astriskSt.top()) {
+                return false;
+            }
+            openSt.pop();
+            astriskSt.pop();
+        }
+        return openSt.empty();
+    }
+};
+
+/// time complexity is o(n)
+// space complexity is O(n)
