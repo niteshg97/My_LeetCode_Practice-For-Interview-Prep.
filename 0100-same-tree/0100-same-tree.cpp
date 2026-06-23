49580b569a6d4ddaf2a0325321f770fb88d30819
Nitesh Kumar <166297874+niteshg97@users.noreply.github.com>
2026-06-23 11:47:31 +0530

    Time: 0 ms (100%), Space: 12.8 MB (47.1%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -9,6 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ // method 1; using the recurssion 
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
