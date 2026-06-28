678db4231a9d4438253eaa9417e4b0b2c8908b8c
Nitesh Kumar <166297874+niteshg97@users.noreply.github.com>
2026-06-28 10:14:04 +0530

    Time: 7 ms (34.77%), Space: 27.2 MB (82.88%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,31 +1,74 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+// pre order ---> root->left->right
+// in order  ---> left-->root-->right
+// post order traversal: right -> root--> left
+// these aare the traversal o traverse the ... pre order traversal
+
+/*
+Approach to solve this problem ..
+first find root in preorder ;
+then find the root index in inorder
+and call a solve function (root->left, start , i-1);
+and for right solve function(root->right, i+1 ,end )
+*/
+
 class Solution {
- public:
-  TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-    unordered_map<int, int> inToIndex;
+public:
+    // idx ko reference (&) se pass kar rahe hain taaki
+    // preorder ka next root har recursive call me update ho.
+    TreeNode* solve(vector<int>& preorder, vector<int>& inorder, int start,
+                    int end, int& idx) {
 
-    for (int i = 0; i < inorder.size(); ++i)
-      inToIndex[inorder[i]] = i;
+        // Base Case:
+        // Agar start end se bada ho gaya,
+        // matlab is subtree me koi node nahi hai.
+        if (start > end)
+            return NULL;
 
-    return build(preorder, 0, preorder.size() - 1, inorder, 0,
-                 inorder.size() - 1, inToIndex);
-  }
+        // Preorder ka current element hi root hoga.
+        int rootVal = preorder[idx];
 
- private:
-  TreeNode* build(const vector<int>& preorder, int preStart, int preEnd,
-                  const vector<int>& inorder, int inStart, int inEnd,
-                  const unordered_map<int, int>& inToIndex) {
-    if (preStart > preEnd)
-      return nullptr;
+        // Agle recursive call ke liye idx badha do.
+        idx++;
 
-    const int rootVal = preorder[preStart];
-    const int rootInIndex = inToIndex.at(rootVal);
-    const int leftSize = rootInIndex - inStart;
+        // Root node bana do.
+        TreeNode* root = new TreeNode(rootVal);
 
-    TreeNode* root = new TreeNode(rootVal);
-    root->left = build(preorder, preStart + 1, preStart + leftSize, inorder,
-                       inStart, rootInIndex - 1, inToIndex);
-    root->right = build(preorder, preStart + leftSize + 1, preEnd, inorder,
-                        rootInIndex + 1, inEnd, inToIndex);
-    return root;
-  }
+        // Inorder me root ki position dhoondo.
+        int i;
+
+        for (i = start; i <= end; i++) {
+            if (inorder[i] == rootVal)
+                break;
+        }
+
+        // Root ke left side wale elements
+        // left subtree banayenge.
+        root->left = solve(preorder, inorder, start, i - 1, idx);
+
+        // Root ke right side wale elements
+        // right subtree banayenge.
+        root->right = solve(preorder, inorder, i + 1, end, idx);
+
+        return root;
+    }
+
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+
+        // Preorder ka first element root hota hai.
+        int idx = 0;
+
+        return solve(preorder, inorder, 0, preorder.size() - 1, idx);
+    }
 };
