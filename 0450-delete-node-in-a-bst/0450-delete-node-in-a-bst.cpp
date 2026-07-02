f36b9d4e9f9189487206ffa23d91efab56d7ec47
Nitesh Kumar <166297874+niteshg97@users.noreply.github.com>
2026-07-02 10:22:22 +0530

    Time: 0 ms (100%), Space: 34.2 MB (87.32%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        // Tree empty hai
+        if (root == NULL)
+            return NULL;
+        // Key chhoti hai -> left subtree me search karo
+        if (root->val > key ) {
+            root->left = deleteNode(root->left, key);
+        }
+        // Key badi hai -> right subtree me search karo
+        else if (key > root->val) {
+            root->right = deleteNode(root->right, key);
+        }
+        // Node mil gaya
+        else {
+            // Case 1:
+            // Left child nahi hai
+            if (root->left == NULL)
+                return root->right;
+            // Case 2:
+            // Right child nahi hai
+            if (root->right == NULL)
+                return root->left;
+            // Case 3:
+            // Dono children exist karte hain
+            // Right subtree ka sabse chhota node dhoondo
+            TreeNode* successor = root->right;
+            while (successor->left != NULL) {
+                successor = successor->left;
+            }
+            // Successor ki value current node me copy kar do
+            root->val = successor->val;
+            // Duplicate successor ko delete kar do
+            root->right = deleteNode(root->right, successor->val);
+        }
+        return root;
+    }
+};
