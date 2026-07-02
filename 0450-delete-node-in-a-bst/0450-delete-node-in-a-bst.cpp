ad135f64244c572e83297e902d1f2e01c1da8749
Nitesh Kumar <166297874+niteshg97@users.noreply.github.com>
2026-07-02 10:44:20 +0530

    Time: 0 ms (100%), Space: 34.2 MB (87.32%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -11,32 +11,41 @@
  */
 class Solution {
 public:
+
     TreeNode* deleteNode(TreeNode* root, int key) {
-        if(root == NULL) return NULL;
-
-        if( root->val > key){
-            root->left = deleteNode(root->left, key );
+        // Tree empty hai
+        if (root == NULL)
+            return NULL;
+        // Key chhoti hai -> left subtree me search karo
+        if (key < root->val) {
+            root->left = deleteNode(root->left, key);
         }
-
-        else if ( root->val < key){
-            root->right  = deleteNode(root->right , key );
+        // Key badi hai -> right subtree me search karo
+        else if (key > root->val) {
+            root->right = deleteNode(root->right, key);
         }
-        else{
-            if(root->left == NULL){
+        // Node mil gaya
+        else {
+            // Case 1:
+            // Left child nahi hai
+            if (root->left == NULL)
                 return root->right;
-            }
-            if(root->right == NULL){
+            // Case 2:
+            // Right child nahi hai
+            if (root->right == NULL)
                 return root->left;
-            }
+            // Case 3:
+            // Dono children exist karte hain
+            // Right subtree ka sabse chhota node dhoondo
             TreeNode* successor = root->right;
-            while(successor->left != NULL){
-                successor = successor->left ;
+            while (successor->left != NULL) {
+                successor = successor->left;
             }
+            // Successor ki value current node me copy kar do
             root->val = successor->val;
-            root->right = deleteNode(root->right,successor->val);
+            // Duplicate successor ko delete kar do
+            root->right = deleteNode(root->right, successor->val);
         }
         return root;
-
-        
     }
 };
