07bc8377c8b669d4944d17df5cb40b422fe31806
Nitesh Kumar <166297874+niteshg97@users.noreply.github.com>
2026-07-06 09:22:58 +0530

    Time: 0 ms (100%), Space: 60.9 MB (52.87%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -19,38 +19,39 @@
  * };
  */
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    // Pehla galat node
-    TreeNode* first = NULL;
-    // Dusra galat node
-    TreeNode* second = NULL;
-    // Previous visited node (Inorder traversal ke liye)
-    TreeNode* prev = NULL;
+    TreeNode* first = nullptr;
+    TreeNode* second = nullptr;
+    TreeNode* prev = nullptr;
+
     void inorder(TreeNode* root) {
-        if (root == NULL)
+        if (root == nullptr)
             return;
-        // Left subtree
         inorder(root->left);
-        // -----------------------------
-        // Check BST property
-        // -----------------------------
-        if (prev != NULL && prev->val > root->val) {
-            // Pehli baar violation mila
-            if (first == NULL) {
+        if (prev != nullptr && prev->val > root->val) {
+
+            if (first == nullptr)
                 first = prev;
-            }
-            // Har violation me second update hota rahega
+
             second = root;
         }
-        // Previous ko update karo
         prev = root;
-        // Right subtree
         inorder(root->right);
     }
     void recoverTree(TreeNode* root) {
         inorder(root);
-        // Dono wrong nodes ke values swap kar do
         swap(first->val, second->val);
     }
 };
