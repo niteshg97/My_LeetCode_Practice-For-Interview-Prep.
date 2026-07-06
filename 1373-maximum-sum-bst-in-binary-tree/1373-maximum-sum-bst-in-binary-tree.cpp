5782c41c2b7e8565aba0fa088b828c74a91c2546
Nitesh Kumar <166297874+niteshg97@users.noreply.github.com>
2026-07-06 10:07:34 +0530

    Time: 643 ms (21.09%), Space: 111.7 MB (33.35%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
@@ -18,49 +18,48 @@
  * };
  */
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-public:
-
-    // Final answer
-    int maxSum = 0;
-
-    // Har subtree ke baare me ye information return hogi
-    struct NodeInfo {
-        bool isBST;     // Kya subtree BST hai?
-        int sum;        // Subtree ka sum
-        int minValue;   // Minimum value
-        int maxValue;   // Maximum value
+    int ans = 0;
+    struct Info{
+        bool isBst;
+        int sum;
+        int mx;
+        int mn;
     };
 
-    NodeInfo solve(TreeNode* root) {
-        // Empty tree is always BST
-        if (root == NULL) {
-            return {true, 0, INT_MAX, INT_MIN};
+    Info find(TreeNode* root){
+        if(root == NULL) return {1, 0, INT_MIN, INT_MAX};
+
+        struct Info left = find(root->left);
+        struct Info right = find(root->right);
+        struct Info curr;
+        if(left.mx < root-> val && right.mn > root->val && left.isBst && right.isBst){
+            curr.isBst = 1;
+            curr.sum = left.sum + right.sum + root->val;
+            curr.mx = max(right.mx, root->val);
+            curr.mn = min(left.mn, root->val);
+            ans = max(ans, curr.sum);
         }
-        // Left subtree ki information
-        NodeInfo left = solve(root->left);
-        // Right subtree ki information
-        NodeInfo right = solve(root->right);
-        // Check karo current subtree BST hai ya nahi
-        if (left.isBST &&
-            right.isBST &&
-            root->val > left.maxValue &&
-            root->val < right.minValue) {
-            int currentSum = left.sum + right.sum + root->val;
-            // Answer update
-            maxSum = max(maxSum, currentSum);
-            return {
-                true,
-                currentSum,
-                min(root->val, left.minValue),
-                max(root->val, right.maxValue)
-            };
+        else{
+            curr.isBst = 0;
+            curr.sum = max(left.sum, right.sum);
         }
-        // BST nahi hai
-        return {false, 0, 0, 0};
+        return curr;
     }
+public:
     int maxSumBST(TreeNode* root) {
-        solve(root);
-        return maxSum;
+        find(root);
+        return ans;
     }
 };
