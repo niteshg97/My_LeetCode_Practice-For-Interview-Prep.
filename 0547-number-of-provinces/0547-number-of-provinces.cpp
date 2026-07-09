854875885cd1bc6521cb67c642b72597041f667f
Nitesh Kumar <166297874+niteshg97@users.noreply.github.com>
2026-07-09 21:48:37 +0530

    Time: 0 ms (100%), Space: 19.4 MB (72.36%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,33 +1,27 @@
 class Solution {
+    private:
+    void dfs(int node , vector<vector<int>> & isConnected , vector<int> &vis){
+      vis[node] = 1;
+      for( int j = 0 ; j < isConnected.size() ; j++){
+        if(isConnected[node][j]==1 && !vis[j]){
+            dfs(j  , isConnected , vis);
+        }
+      }
+
+    }
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        int count = 0 ;
-        vector<int> vis(n,0);
-        
-        for(int i = 0; i<n ; i++){
+        vector<int>vis(n, 0);
+        int count = 0;
+
+        for( int i = 0 ; i< n ; i++){
             if(!vis[i]){
-                count ++;
-                queue<int>q;
-                q.push(i);
-                vis[i] = 1;
-
-                while(!q.empty()){
-                    int node  = q.front();
-                    q.pop();
-                     
-                 // Traverse all possible neighbours of 'node'
-                 for( int j = 0 ; j < n ; j++ ){
-                    if(isConnected[node][j] == 1 && ! vis[j]){
-                        vis[j]= 1;
-                        q.push(j);
-                    }
-                 }
-
-                }
+            count ++ ;
+            dfs(i , isConnected , vis);
             }
         }
-        return count; 
+        return count;
         
     }
 };
