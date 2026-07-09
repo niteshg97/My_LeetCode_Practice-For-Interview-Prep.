a85f9fa5557748c095a584dd385c0fade72a4d07
Nitesh Kumar <166297874+niteshg97@users.noreply.github.com>
2026-07-09 21:36:05 +0530

    Time: 0 ms (100%), Space: 19.9 MB (46.24%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-
         int n = isConnected.size();
-
-        vector<int> vis(n, 0);
-        int provinces = 0;
-
-        for (int i = 0; i < n; i++) {
-
-            if (!vis[i]) {
-
-                provinces++;
-
-                queue<int> q;
+        int count = 0 ;
+        vector<int> vis(n,0);
+        
+        for(int i = 0; i<n ; i++){
+            if(!vis[i]){
+                count ++;
+                queue<int>q;
                 q.push(i);
                 vis[i] = 1;
 
-                while (!q.empty()) {
-
-                    int node = q.front();
+                while(!q.empty()){
+                    int node  = q.front();
                     q.pop();
-
-                    // Traverse all possible neighbours of 'node'
-                    for (int j = 0; j < n; j++) {
-
-                        if (isConnected[node][j] == 1 && !vis[j]) {
-                            vis[j] = 1;
-                            q.push(j);
-                        }
+                     
+                 // Traverse all possible neighbours of 'node'
+                 for( int j = 0 ; j < n ; j++ ){
+                    if(isConnected[node][j] == 1 && ! vis[j]){
+                        vis[j]= 1;
+                        q.push(j);
                     }
+                 }
+
                 }
             }
         }
-
-        return provinces;
+        return count; 
+        
     }
 };
