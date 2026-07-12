fc81935d365a42f4736e56d47bccfe68aac46740
Nitesh Kumar <166297874+niteshg97@users.noreply.github.com>
2026-07-12 16:28:38 +0530

    Time: 443 ms (91.85%), Space: 155 MB (11.11%) - LeetHub

--- 2157-groups-of-strings/2157-groups-of-strings.cpp
+++ 2157-groups-of-strings/2157-groups-of-strings.cpp
@@ -1,84 +1,84 @@
 class Solution {
 public:
-    int groups, largest;
+    vector<int> parent;
+    vector<int> group_size;
+    int num_groups;
+    int max_size;
 
-    int find(int x, unordered_map<int, int>& parent) {
-        if (parent[x] == x)
-            return x;
-
-        return parent[x] = find(parent[x], parent);
+    int find(int i) {
+        if (parent[i] == i)
+            return i;
+        return parent[i] = find(parent[i]);
     }
 
-    void unite(int a, int b,
-               unordered_map<int, int>& parent,
-               unordered_map<int, int>& sz) {
-
-        if (!parent.count(b))
-            return;
-
-        int pa = find(a, parent);
-        int pb = find(b, parent);
-
-        if (pa == pb)
-            return;
-
-        parent[pa] = pb;
-        sz[pb] += sz[pa];
-
-        largest = max(largest, sz[pb]);
-        groups--;
+    void unite(int i, int j) {
+        int root_i = find(i);
+        int root_j = find(j);
+        if (root_i != root_j) {
+            parent[root_i] = root_j;
+            group_size[root_j] += group_size[root_i];
+            max_size = max(max_size, group_size[root_j]);
+            num_groups--;
+        }
     }
 
     vector<int> groupStrings(vector<string>& words) {
+        unordered_map<int, int> mask_to_id;
+        vector<int> mask_counts;
+        int id_counter = 0;
 
-        unordered_map<int, int> parent;
-        unordered_map<int, int> sz;
-
-        groups = words.size();
-        largest = 0;
-
-        // Build DSU nodes (one per unique mask)
-        for (string &word : words) {
-
+        for (const string& word : words) {
             int mask = 0;
-
-            for (char c : word)
-                mask |= (1 << (c - 'a'));
-
-            parent[mask] = mask;
-            sz[mask]++;
-
-            largest = max(largest, sz[mask]);
-
-            // Duplicate mask => same group
-            if (sz[mask] > 1)
-                groups--;
+            for (char ch : word) {
+                mask |= (1 << (ch - 'a'));
+            }
+            if (mask_to_id.find(mask) == mask_to_id.end()) {
+                mask_to_id[mask] = id_counter++;
+                mask_counts.push_back(1);
+            } else {
+                mask_counts[mask_to_id[mask]]++;
+            }
         }
 
-        // Connect masks
-        for (auto &[mask, _] : parent) {
+        int n = id_counter;
+        parent.resize(n);
+        group_size = mask_counts;
+        num_groups = n;
+        max_size = 0;
 
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            if (group_size[i] > 0) {
+                max_size = max(max_size, group_size[i]);
+            }
+        }
+
+        unordered_map<int, int> delete_intermediates;
+
+        for (auto const& [mask, id] : mask_to_id) {
             for (int i = 0; i < 26; i++) {
+                if ((mask & (1 << i)) != 0) {
+                    int del_mask = mask ^ (1 << i);
+                    
+                    if (mask_to_id.find(del_mask) != mask_to_id.end()) {
+                        unite(id, mask_to_id[del_mask]);
+                    }
 
-                // Add / Remove
-                unite(mask, mask ^ (1 << i), parent, sz);
-
-                // Replace
-                if ((mask >> i) & 1) {
-
-                    for (int j = 0; j < 26; j++) {
-
-                        if (((mask >> j) & 1) == 0) {
-
-                            int newMask = (mask ^ (1 << i)) | (1 << j);
-
-                            unite(mask, newMask, parent, sz);
-                        }
+                    if (delete_intermediates.find(del_mask) != delete_intermediates.end()) {
+                        unite(id, delete_intermediates[del_mask]);
+                    } else {
+                        delete_intermediates[del_mask] = id;
+                    }
+                } 
+                else {
+                    int add_mask = mask | (1 << i);
+                    if (mask_to_id.find(add_mask) != mask_to_id.end()) {
+                        unite(id, mask_to_id[add_mask]);
                     }
                 }
             }
         }
 
-        return {groups, largest};
+        return {num_groups, max_size};
     }
 };
