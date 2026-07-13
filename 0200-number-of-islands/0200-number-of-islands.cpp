bf22e640cf395c6ad703a1299141115217d8012e
Nitesh Kumar <166297874+niteshg97@users.noreply.github.com>
2026-07-13 10:28:08 +0530

    Time: 15 ms (99.47%), Space: 15.9 MB (99.74%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid , int row , int col){
-        int m = grid.size(); // rows 
-        int n = grid[0].size(); // column ...
-
-        if(row<0 || row>=m || col<0 || col>=n ||  grid[row][col]!='1') {
+    void dfs(int i, int j, vector<vector<char>>& grid)
+    {
+        int n=grid.size();
+        int m=grid[0].size();
+        if(i<0 || j<0 || i>=n || j>=m || grid[i][j]=='0')
             return;
-        }
-        
-        grid[row][col]='$'; // mark visited
-
-        dfs(grid, row-1, col);
-        dfs(grid, row, col+1);
-        dfs(grid, row+1, col);
-        dfs(grid, row, col-1);
+        grid[i][j]='0';
+        dfs(i-1,j,grid);
+        dfs(i,j-1,grid);
+        dfs(i,j+1,grid);
+        dfs(i+1,j,grid);
     }
-    int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size(); // rows 
-        int n = grid[0].size(); // column ...
-        int island= 0;
-        for(int i =0 ; i<m;i++){
-            for (int j =0; j<n;j++){
-                if(grid[i][j]== '1'){
-                    dfs(grid, i , j);
-                    island++;
+    int numIslands(vector<vector<char>>& grid) 
+    {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        int n=grid.size();
+        int m=grid[0].size();
+        int i,j;
+        int c=0;
+        for(i=0;i<n;i++)
+            for(j=0;j<m;j++)
+                if(grid[i][j]=='1')
+                {
+                    dfs(i, j, grid);
+                    c++;
                 }
-            }
-        }
-        return island;
+        return c;
     }
 };
