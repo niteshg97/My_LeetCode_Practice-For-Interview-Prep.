59766c4fe876bdd1e94ddc0d29c005a387176905
Nitesh Kumar <166297874+niteshg97@users.noreply.github.com>
2026-07-16 10:45:37 +0530

    Time: 8 ms (83.21%), Space: 61.1 MB (77.65%) - LeetHub

---
+++ 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    vector<int> topoSort(int k, vector<vector<int>>& conditions) {
+        vector<vector<int>> adj(k + 1);
+        vector<int> indegree(k + 1, 0);
+        // Build graph
+        for (auto& edge : conditions) {
+            int u = edge[0];
+            int v = edge[1];
+            adj[u].push_back(v);
+            indegree[v]++;
+        }
+        queue<int> q;
+        for (int i = 1; i <= k; i++) {
+            if (indegree[i] == 0)
+                q.push(i);
+        }
+        vector<int> order;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            order.push_back(node);
+            for (int neigh : adj[node]) {
+                indegree[neigh]--;
+                if (indegree[neigh] == 0)
+                    q.push(neigh);
+            }
+        }
+        // Cycle exists
+        if (order.size() != k)
+            return {};
+        return order;
+    }
+    vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions,
+                                    vector<vector<int>>& colConditions) {
+        vector<int> rowOrder = topoSort(k, rowConditions);
+        if (rowOrder.empty())
+            return {};
+        vector<int> colOrder = topoSort(k, colConditions);
+        if (colOrder.empty())
+            return {};
+        vector<int> rowPos(k + 1);
+        vector<int> colPos(k + 1);
+        for (int i = 0; i < k; i++) {
+            rowPos[rowOrder[i]] = i;
+            colPos[colOrder[i]] = i;
+        }
+        vector<vector<int>> ans(k, vector<int>(k, 0));
+        for (int num = 1; num <= k; num++) {
+            ans[rowPos[num]][colPos[num]] = num;
+        }
+        return ans;
+    }
+};
