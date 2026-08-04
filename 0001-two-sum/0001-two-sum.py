33c0429435d49cf1e558fd21b3923cf9adbcd094
Nitesh Kumar <166297874+niteshg97@users.noreply.github.com>
2026-08-04 10:58:39 +0530

    Time: 0 ms (100%), Space: 20.5 MB (40.42%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,17 @@
+## Method -1 using the hashMap
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range( i+1 , len(nums)):
-                if(nums[i]+nums[j] == target):
-                    return [i , j]
+        mp = {} # num , Index
+        for i , num in  enumerate(nums):
+            diff = target - num
+            if diff in mp:
+                return [mp[diff], i]
+
+            mp[num] =  i ## agar nahi hai toh map me num and its index daal do 
+            
+
+
+
+
+
         
