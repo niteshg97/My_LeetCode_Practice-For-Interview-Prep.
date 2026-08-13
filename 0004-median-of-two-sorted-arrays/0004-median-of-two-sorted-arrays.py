76273bf17e72aa849a691150b56a7306e3284581
Nitesh Kumar <166297874+niteshg97@users.noreply.github.com>
2026-08-13 10:32:54 +0530

    Time: 0 ms (100%), Space: 19.5 MB (76.95%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,21 +1,52 @@
-    # merge two sortd arrray , arrange in increasing order ..
-    # midian is  the mid of the array 
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        #sort the  array  and merge and
-        array = []
-        array = nums1 + nums2
-        array.sort()
-        n = len(array)
-        for i in range(n):
-            if((n%2) == 0): # array is even
-                midi = array[n//2 - 1] + array[(n//2)]
-                return midi/2
+        # Always binary search on the smaller array
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+        m = len(nums1)
+        n = len(nums2)
+        l = 0
+        r = m
+        while l <= r:
+
+            partition1 = (l + r) // 2
+            partition2 = (m + n + 1) // 2 - partition1
+
+            # Left and right values
+            if partition1 == 0:
+                left1 = float('-inf')
             else:
-                midi = array[n//2]
-                return midi 
+                left1 = nums1[partition1 - 1]
 
-       
-        
+            if partition1 == m:
+                right1 = float('inf')
+            else:
+                right1 = nums1[partition1]
 
-        
+            if partition2 == 0:
+                left2 = float('-inf')
+            else:
+                left2 = nums2[partition2 - 1]
+
+            if partition2 == n:
+                right2 = float('inf')
+            else:
+                right2 = nums2[partition2]
+
+            # Correct partition
+            if left1 <= right2 and left2 <= right1:
+                # Odd
+                if (m + n) % 2 == 1:
+                    return max(left1, left2)
+                # Even
+                else:
+                    return (max(left1, left2) +
+                         min(right1, right2)) / 2
+            # We took too many elements from nums1
+            elif left1 > right2:
+                r = partition1 - 1
+            # We took too few elements from nums1
+            else:
+                l = partition1 + 1
+
+
