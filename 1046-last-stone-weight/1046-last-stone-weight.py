496fa71e0849cf5790f62d0044354acb0f714077
Nitesh Kumar <166297874+niteshg97@users.noreply.github.com>
2026-09-09 12:45:04 +0530

    Time: 40 ms (0.09%), Space: 19.2 MB (97.91%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,20 +1,4 @@
-class Solution:
-    def lastStoneWeight(self, stones: List[int]) -> int:
-        heap = []
-        for stone in stones:
-            heapq.heappush(self.heap , -stone) ## heaviest element ko heap me daal do 
-        while(len(heap) > 1):
-            stone1 = -heapq.heappop(heap)
-            stone2 = -heapq.heappop(heap)
-
-            if(len(self.heap) >= 3):
-                heap.heappop(self.heap)
-            stone1 = self.heap[0]
-            stone2 = self.heap[1]
-            if(stone1!= stone2):
-                heapq.heappush(-(stone1 - stone2))
-
-            
+   
 class Solution:
 
     def lastStoneWeight(self, stones: List[int]) -> int:
