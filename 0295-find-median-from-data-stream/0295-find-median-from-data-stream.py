4e176cfae75a61e31b75ff885dcad54181408529
Nitesh Kumar <166297874+niteshg97@users.noreply.github.com>
2026-09-15 12:40:18 +0530

    Time: 179 ms (56.74%), Space: 43 MB (14.99%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,11 +1,11 @@
 class MedianFinder:
 
     def __init__(self):
-        self.max_heap = []
-        self.min_heap = []
+        self.max_heap = [] #left part of num will store
+        self.min_heap = [] #right part of num will store
 
     def addNum(self, num: int) -> None:
-        heapq.heappush(self.max_heap , -num) # put no in max heap 
+        heapq.heappush(self.max_heap , -num) # put num in max heap 
         largest_small = -heapq.heappop(self.max_heap)
         heapq.heappush(self.min_heap , largest_small)
         if(len(self.min_heap) > len(self.max_heap)):
